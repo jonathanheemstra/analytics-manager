b56bb4d98048ab64455e9f82b5a36b09654dc787
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-11-29 16:15:06 -0800

    Add signin button add sign in button and styles for "home" page

--- scripts/model/accountList.js
+++ scripts/model/accountList.js
@@ -1,29 +1,30 @@
 'use strict';
 (function(module) {
-  var accountList = {};
+  const accountList = {};
   accountList.allAccounts = [];
   accountList.accountIDs = [];
+  accountList.allAccountUsers = [];
 
   // Replace with your client ID from the developer console.
-  var CLIENT_ID = '90425361918-ve2unlo91glh26eqai2jstvcpr963qpo.apps.googleusercontent.com';
+  const CLIENT_ID = '90425361918-ve2unlo91glh26eqai2jstvcpr963qpo.apps.googleusercontent.com';
 
   // Set authorized scope.
   // var SCOPES = ['https://www.googleapis.com/auth/analytics.edit'];
-  var SCOPES = ['https://www.googleapis.com/auth/analytics.manage.users'];
+  const SCOPES = ['https://www.googleapis.com/auth/analytics.manage.users'];
 
 
   function authorize(event) {
     // Handles the authorization flow.
     // `immediate` should be false when invoked from the button click.
-    var useImmdiate = event ? false : true;
-    var authData = {
+    let useImmdiate = event ? false : true;
+    let authData = {
       client_id: CLIENT_ID,
       scope: SCOPES,
       immediate: useImmdiate
     };
 
     gapi.auth.authorize(authData, function(response) {
-      var authButton = document.getElementById('auth-button');
+      const authButton = document.getElementById('auth-button');
       if (response.error) {
         authButton.hidden = false;
       } else {
@@ -36,16 +37,15 @@
   function queryAccounts() {
   // Load the Google Analytics client library.
     gapi.client.load('analytics', 'v3').then(function() {
-
     // Get a list of all Google Analytics accounts for this user
-      var request = gapi.client.analytics.management.accounts.list();
+      const request = gapi.client.analytics.management.accounts.list();
       request.execute(printAccounts);
     });
   }
 
   function printAccounts(results) {
     if (results && !results.error) {
-      var accounts = results.items;
+      const accounts = results.items;
       accounts.forEach(function(account) {
         return accountList.allAccounts.push(account);
       });
@@ -59,7 +59,7 @@
   }
 
   function listAccountUserLinks(accountID) {
-    var request = gapi.client.analytics.management.accountUserLinks.list({
+    const request = gapi.client.analytics.management.accountUserLinks.list({
       'accountId': accountID
     });
     request.execute(printAccountUserLinks);
@@ -67,21 +67,13 @@
 
   function printAccountUserLinks(results) {
     if (results && !results.error) {
-      var accountLinks = results.items;
+      const accountLinks = results.items;
       for (var i = 0, accountUserLink; accountUserLink = accountLinks[i]; i++) {
-        var entity = accountUserLink.entity;
-        var accountRef = entity.accountRef;
-        var userRef = accountUserLink.userRef;
-        var permissions = accountUserLink.permissions;
-
-        console.log('Account User Link Id: ' + accountUserLink.id);
-        console.log('Account User Link Kind: ' + accountUserLink.kind);
-        console.log('User Email: ' + userRef.email);
-        console.log('Permissions effective: ' + permissions.effective);
-        console.log('Permissions local: ' + permissions.local);
-        console.log('Account Id: ' + accountRef.id);
-        console.log('Account Kind: ' + accountRef.kind);
-        console.log('Account Name: ' + accountRef.name);
+        // var entity = accountUserLink.entity;
+        // var accountRef = entity.accountRef;
+        // var userRef = accountUserLink.userRef;
+        // var permissions = accountUserLink.permissions;
+        accountList.allAccountUsers.push(accountUserLink);
       }
     }
   }
