e2521c2fcff6f115692b6c83bdbffcf0070cb19f
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-11-29 18:17:15 -0800

    start handlebars integration start setting up handlebars to receive and print account data to accounts page.

--- scripts/view/accountListView.js
+++ scripts/view/accountListView.js
@@ -1,6 +1,30 @@
 'use strict';
 (function(module) {
-  var accountListView = {};
+  const accountListView = {};
+
+  const $signIn = $('#sign-in');
+  const $accountPrintOut = $('#account-printout');
+  const $accountUsers = $('.account-users');
+  const $expand = $('.expand');
+  const accountTableTemplate = Handlebars.compile($('#account-table-template').text());
+
+  accountListView.renderTable = function(data) {
+    let source = $('#account-table-template').html();
+    let template = Handlebars.compile(source);
+    return template(data);
+  };
+
+  $accountPrintOut.hide();
+  $accountUsers.hide();
+
+
+  $signIn.on('click', 'button', function() {
+    $signIn.hide();
+    $accountPrintOut.fadeIn();
+  });
+  $expand.on('click', function(){
+    $accountUsers.show();
+  });
 
   module.accountListView = accountListView;
 }(window));
